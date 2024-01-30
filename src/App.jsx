364441846c35cf9cace04bea6bb6430073b0fee7
Weaver <cordies1106@gmail.com>
2024-01-30 22:12:39 +0200

    finally managed the startup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Button>11111</Button>
+    </>
+  )
+}
+
+export default App
