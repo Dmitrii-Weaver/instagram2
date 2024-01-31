d79b45cbefa810b1bccccda2b00c80ca015e2ff1
Weaver <cordies1106@gmail.com>
2024-02-01 00:59:42 +0200

    auth input handling added, sidebar started

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import AuthPage from './pages/AuthPage/AuthPage'
+import PageLayout from './Layouts/PageLayouts/pageLayout'
 
 function App() {
 
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   )
 }
 
--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,19 +1,42 @@
 import { Box, Button, Flex, Image, Input, Text, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AuthForm = () => {
     const [isLogin, setIsLogin] = useState(true)
+    const navigate = useNavigate()
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const handleAuth = () => {
+        if(!inputs.email || !inputs.password ){
+            alert("information missing")
+            return
+        }
+        navigate("/")
+    }
+
     return (
         <>
             <Box border={"1px solid gray"} borderRadius={4} padding={5}>
                 <VStack spacing={4}>
                     <Image src='/logo.png' h={24} cursor={"pointer"} alt='instagram' />
-                    <Input placeholder='Email' type='email' fontSize={14} ></Input>
-                    <Input placeholder='Password' type='password' fontSize={14} ></Input>
 
-                    {!isLogin ? (<Input placeholder='Confirm password' type='password' fontSize={14} ></Input>) : null}
+                    <Input placeholder='Email' type='email' fontSize={14}
+                        value={inputs.email}
+                        onChange={(e) => setInputs({ ...inputs, email: e.target.value })} />
+                    <Input placeholder='Password' type='password' fontSize={14}
+                        value={inputs.password}
+                        onChange={(e) => setInputs({ ...inputs, password: e.target.value })} />
 
-                    <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}> {isLogin ? "Login" : "Sign up"} </Button>
+                    {!isLogin ? (<Input placeholder='Confirm password' type='password' fontSize={14}
+                        value={inputs.confirmPassword}
+                        onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })} />) : null}
+
+                    <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} onClick={handleAuth}> {isLogin ? "Login" : "Sign up"} </Button>
 
 
                     <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
@@ -34,7 +57,7 @@ const AuthForm = () => {
                     <Box mx={2} fontSize={14}>
                         {isLogin ? "Don't have an account?" : "Already have an account?"}
                     </Box>
-                    <Box onClick={()=> setIsLogin(!isLogin)} color={'blue.500'} cursor={"pointer"}>
+                    <Box onClick={() => setIsLogin(!isLogin)} color={'blue.500'} cursor={"pointer"}>
                         {isLogin ? "Sign up" : "Log in"}
                     </Box>
 
