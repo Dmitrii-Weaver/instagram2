2f08b386336566da62aac1de76abec4a734a1710
Weaver <cordies1106@gmail.com>
2024-02-05 23:03:36 +0200

    Started working on Homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,20 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
+import Feedposts from '../../components/Feedposts/Feedposts'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10}  border={"1px solid green"}>
+          <Feedposts />
+        </Box>
+        <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"} border={"1px solid red"}>
+          Suggested Users
+        </Box>
+      </Flex>
+
+    </Container>
   )
 }
 
