b5722629c578e42c70be5b88ac3f2b7371bfa903
Weaver <cordies1106@gmail.com>
2024-02-06 20:48:59 +0200

    Update Postfooter.jsx

--- src/components/Feedposts/Postfooter.jsx
+++ src/components/Feedposts/Postfooter.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { NotificationsLogo, SearchLogo, UnlikeLogo } from '../../assets/constants'
 
 const Postfooter = () => {
+  const [liked, setLiked] = useState(false)
+  const [likes, setLikes] = useState(120)
+
+  const handleLike = () => {
+    if(liked){
+      setLiked(false)
+      setLikes(likes-1)
+    }
+    else{
+      setLiked(true)
+      setLikes(likes+1)
+    }
+  }
+
   return (
-    <div>Postfooter</div>
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box onClick={handleLike}>
+          {!liked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
+        </Box>
+      </Flex>
+    </>
   )
 }
 
