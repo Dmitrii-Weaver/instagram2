7c9e6dcd5862c0546c409e367f98b2bb19e09a08
Weaver <cordies1106@gmail.com>
2024-02-07 20:27:06 +0200

    Posts improved

--- src/components/Feedposts/Feedpost.jsx
+++ src/components/Feedposts/Feedpost.jsx
@@ -7,7 +7,7 @@ const Feedpost = () => {
     return (
         <>
         <Postheader />
-        <Box>
+        <Box my={2}>
             <Image src='/img1.png' alt="alt"/>
         </Box>
         <Postfooter />
