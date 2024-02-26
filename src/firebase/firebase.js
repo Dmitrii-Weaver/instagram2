6f4b7126e6c965933dc472f767922ba60b3b96bb
Weaver <cordies1106@gmail.com>
2024-02-26 03:00:59 +0200

    sign up refactoring started

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBL54vqP_YkZ2PsNyFZLfBovlnHQmcV2Gw",
-  authDomain: "insta-clone-76ea9.firebaseapp.com",
-  projectId: "insta-clone-76ea9",
-  storageBucket: "insta-clone-76ea9.appspot.com",
-  messagingSenderId: "1043098052901",
-  appId: "1:1043098052901:web:2b84256cbec5332df9d2cc"
+    apiKey: "AIzaSyBL54vqP_YkZ2PsNyFZLfBovlnHQmcV2Gw",
+    authDomain: "insta-clone-76ea9.firebaseapp.com",
+    projectId: "insta-clone-76ea9",
+    storageBucket: "insta-clone-76ea9.appspot.com",
+    messagingSenderId: "1043098052901",
+    appId: "1:1043098052901:web:2b84256cbec5332df9d2cc"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ const auth = getAuth(app)
 const firestore = getFirestore(app)
 const storage = getStorage(app)
 
-export {app, auth, firestore, storage}
+export { app, auth, firestore, storage }
