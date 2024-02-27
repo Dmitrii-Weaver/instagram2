48789a69663ad100aab8d210831ef3115e06d91d
Weaver <cordies1106@gmail.com>
2024-02-28 01:51:24 +0200

    SignOut Hook init

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
-import { Avatar, Box, Flex, Link, Tooltip } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, Link, Tooltip } from '@chakra-ui/react'
 import { Link as RouterLink } from "react-router-dom"
 import React from 'react'
 import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from '../../assets/constants'
 import { AiFillHome } from "react-icons/ai"
 import { BiLogOut } from "react-icons/bi"
+import useLogout from '../../hooks/useLogout'
 
 
 /* logos import */
@@ -33,6 +34,8 @@ const Sidebar = () => {
     }
   ]
 
+  const {handleLogout, isLoggingOut} = useLogout()
+
   return (
     <Box h={"100vh"} borderRight={"1px solid"} borderColor={"WhiteAlpha.300"} py={8} position={"sticky"} top={0} left={0} px={{ base: 2, md: 4 }}>
 
@@ -60,10 +63,10 @@ const Sidebar = () => {
         {/* Sidebar bottom*/}
 
         <Tooltip hasArrow label={"Logout"} placement='right' ml={1} openDelay={500} display={{ base: 'block' }}>
-          <Link marginTop={"auto"} display={"flex"} to={"/auth"} as={RouterLink} alignItems={"center"} gap={4} _hover={{ bg: "whiteAlpha.400" }} borderRadius={6} p={2} w={{ base: 10, md: "full" }} justifyContent={{ base: "center", md: "flex-start" }}>
+          <Flex onClick={handleLogout} marginTop={"auto"} alignItems={"center"} gap={4} _hover={{ bg: "whiteAlpha.400" }} borderRadius={6} p={2} w={{ base: 10, md: "full" }} justifyContent={{ base: "center", md: "flex-start" }}>
             <BiLogOut size={25} />
-            <Box display={{ base: "none", md: "block" }}> Logout </Box>
-          </Link>
+            <Button display={{ base: "none", md: "block" }} variant={"ghost"} _hover={{bg:"transparent"}} isLoading={isLoggingOut}> Logout </Button>
+          </Flex>
         </Tooltip>
       </Flex>
     </Box>
