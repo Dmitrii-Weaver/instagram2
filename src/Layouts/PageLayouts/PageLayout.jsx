896f834cc1231193160ea4d122aae1ab6db29af9
Weaver <cordies1106@gmail.com>
2024-03-01 01:14:01 +0200

    navbar added, some optimization made

--- src/Layouts/PageLayouts/PageLayout.jsx
+++ src/Layouts/PageLayouts/PageLayout.jsx
@@ -1,24 +1,31 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Spinner } from '@chakra-ui/react'
 import React from 'react'
 import Sidebar from '../../components/Sidebar/sideBar'
 import { useLocation } from 'react-router-dom'
 import { auth } from '../../firebase/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import Navbar from '../../components/Navbar/Navbar'
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation()
-  const [user] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth)
   const canRenderSidebar = pathname !== "/auth" && user
+  const canRenderNavbar = !user && !loading && pathname !== "/auth"
+
+  const checkingUser = !user && loading
+  if(checkingUser) return <PageLayoutSpinner />
+
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/*sidebar*/}
       {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
           <Sidebar />
         </Box>) : null}
-
+      {/*navbar*/}
+      {canRenderNavbar ? <Navbar /> : null}
       {/*main --- content*/}
-      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }} mx={"auto"}>
         {children}
       </Box>
     </Flex>
@@ -26,3 +33,11 @@ const PageLayout = ({ children }) => {
 }
 
 export default PageLayout
+
+const PageLayoutSpinner = () => {
+  return(
+    <Flex flexDir={"column"} h={"100vh"} alignItems={"center"} justifyContent={"center"}>
+      <Spinner size={"xl"} />
+    </Flex>
+  )
+}
