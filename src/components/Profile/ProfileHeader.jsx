3a6f72afd0d0157724760a97dca76d45fe423dd7
Weaver <cordies1106@gmail.com>
2024-03-11 09:09:35 +0200

    profile page reacts to auth/unauth access

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,11 +1,18 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import useUserProfileStore from '../../store/userProfileStore'
+import useAuthStore from "../../store/authStore"
 
 //profile header, got the main profile info
 
 const ProfileHeader = () => {
-    const {userProfile} = useUserProfileStore()
+    const { userProfile } = useUserProfileStore()
+    const authUser = useAuthStore(state => state.user)
+    const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username
+    const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
+
+
+
 
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
@@ -15,9 +22,17 @@ const ProfileHeader = () => {
             <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
                 <Flex gap={4} direction={{ base: "column", sm: "row" }} justifyContent={{ base: "center", sm: "flex-start" }} alignItems={"center"} w={"full"}>
                     <Text fontSize={{ base: "sm", md: "lg" }}>{userProfile.username}</Text>
-                    <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+
+                    {visitingOwnProfileAndAuth && <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
                         <Button bg={"white"} color={"black"} _hover={{ bg: "whiteAlpha.800" }} size={{ base: "xs", md: "sm" }}>Edit Profile</Button>
-                    </Flex>
+                    </Flex>}
+
+                    {visitingAnotherProfileAndAuth && <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                        <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }}>Follow
+                        </Button>
+                    </Flex>}
+
+
                 </Flex>
                 <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
                     <Text fontSize={{ base: "xs", md: "sm" }}>
