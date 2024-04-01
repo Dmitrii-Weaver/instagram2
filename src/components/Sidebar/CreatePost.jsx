897cb5e08dfdc8d6bf4aa371ea0d0bbec629850e
Weaver <cordies1106@gmail.com>
2024-04-01 18:01:06 +0300

    post creation func started

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,7 +1,11 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Textarea, Tooltip, useDisclosure } from "@chakra-ui/react";
 import { CreatePostLogo } from "../../assets/constants";
+import { BsFillImageFill } from "react-icons/bs";
 
 const CreatePost = () => {
+    
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <>
             <Tooltip
@@ -25,6 +29,29 @@ const CreatePost = () => {
                     <Box display={{ base: "none", md: "block" }}>Create</Box>
                 </Flex>
             </Tooltip>
+
+            <Modal isOpen={isOpen} onClose={onClose} size='xl'>
+                <ModalOverlay />
+
+                <ModalContent bg={"black"} border={"1px solid gray"}>
+                    <ModalHeader>Create Post</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Textarea placeholder='Post caption...' />
+
+                        <Input type='file' hidden />
+
+                        <BsFillImageFill
+                            style={{ marginTop: "15px", marginLeft: "5px", cursor: "pointer" }}
+                            size={16}
+                        />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button mr={3}>Post</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     );
 };
