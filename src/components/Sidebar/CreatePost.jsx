fba46aa4451bd7449a57959acb535a836e24cd47
Weaver <cordies1106@gmail.com>
2024-04-08 03:44:39 +0300

    create posts functional

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -3,6 +3,14 @@ import { CreatePostLogo } from "../../assets/constants";
 import { BsFillImageFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import usePreviewImage from "../../hooks/usePreviewImage";
+import useShowToast from "../../hooks/useShowToast";
+import useAuthStore from "../../store/authStore";
+import usePostStore from "../../store/postStore";
+import useUserProfileStore from "../../store/userProfileStore";
+import { useLocation } from "react-router-dom";
+import { addDoc, arrayUnion, collection, doc, updateDoc } from "firebase/firestore";
+import { firestore, storage } from "../../firebase/firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const CreatePost = () => {
 
@@ -10,7 +18,20 @@ const CreatePost = () => {
     const [caption, setCaption] = useState("")
     const imageRef = useRef(null)
     const { selectedFile, handleImageChange, setSelectedFile } = usePreviewImage()
+    const { isLoading, handleCreatePost } = useCreatePost()
+    const showToast = useShowToast()
 
+
+    const handlePostCreation = async () => {
+        try {
+            await handleCreatePost(selectedFile, caption)
+            onClose()
+            setCaption("")
+            setSelectedFile(null)
+        } catch (error) {
+            showToast("Error", error.message, "error")
+        }
+    }
     return (
         <>
             <Tooltip
@@ -48,23 +69,23 @@ const CreatePost = () => {
                             value={caption}
                             onChange={(e) => setCaption(e.target.value)} />
 
-                        <Input type='file' hidden ref={imageRef} onChange={handleImageChange}/>
+                        <Input type='file' hidden ref={imageRef} onChange={handleImageChange} />
 
                         <BsFillImageFill
-                            onClick={()=>imageRef.current.click() }
+                            onClick={() => imageRef.current.click()}
                             style={{ marginTop: "15px", marginLeft: "5px", cursor: "pointer" }}
                             size={16}
                         />
                         {selectedFile && (
                             <Flex mt={5} w={"full"} position={"relative"} justifyContent={"center"}>
-                                <Image src={selectedFile} alt="selimage"/>
-                                <CloseButton position={"absolute"} top={2} right={2} onClick={() => {setSelectedFile("")}}/>
+                                <Image src={selectedFile} alt="selimage" />
+                                <CloseButton position={"absolute"} top={2} right={2} onClick={() => { setSelectedFile("") }} />
                             </Flex>
                         )}
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button mr={3}>Post</Button>
+                        <Button mr={3} onClick={handlePostCreation} isLoading={isLoading}>Post</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
@@ -73,3 +94,51 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+
+function useCreatePost() {
+    const showToast = useShowToast()
+    const [isLoading, setIsLoading] = useState(false)
+    const authUser = useAuthStore((state) => state.user)
+    const createPost = usePostStore(state => state.createPost)
+    const addPost = useUserProfileStore(state => state.addPost)
+    const { pathname } = useLocation()
+
+    const handleCreatePost = async (selectedFile, caption) => {
+        if (!selectedFile) throw new Error("please select an image")
+        setIsLoading(true)
+        const newPost = {
+            caption: caption,
+            likes: [],
+            comments: [],
+            createdAt: Date.now(),
+            createdBy: authUser.uid,
+        }
+
+        try {
+            const postDocRef = await addDoc(collection(firestore, "posts"), newPost)
+            const userDocRef = await doc(firestore, "users", authUser.uid)
+            const imageRef = ref(storage, `posts/${postDocRef.id}`)
+            await updateDoc(userDocRef, { posts: arrayUnion(postDocRef.id) })
+            await uploadString(imageRef, selectedFile, "data_url")
+            const downloadURL = await getDownloadURL(imageRef)
+            await updateDoc(postDocRef, { imageURL: downloadURL })
+
+            newPost.imageURL = downloadURL
+
+            createPost({ ...newPost, id: postDocRef.id })
+            addPost({ ...newPost, id: postDocRef.id })
+
+            showToast("Success", "Post created successfully", "success")
+
+        } catch (error) {
+            showToast("Error", error.message, "error")
+        } finally {
+            setIsLoading(false)
+        }
+
+
+    }
+
+    return { isLoading, handleCreatePost }
+
+}
